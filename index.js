97a5301bc9b177a9b20c9045c27e6812d9106c1f
Amanda Casten <ascasten@gmail.com>
2020-06-29 21:57:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ function add(a, b){
   console.log(sum)
   return sum
 }
+
+function subtract(a,b){
+  var difference = b-a
+  console.log(difference)
+  return difference
+}
+
+function multiply(a,b) {
+  var product = a*b
+  console.log(product)
+  return product
+}
